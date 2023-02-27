9cc685d1fc624c752f86614caca489200feb6be6
narielponce <narielponce@gmail.com>
2023-02-27 19:35:29 -0300

    Agregado contenido CV Redes Blog y Contact

--- js/js.js
+++ js/js.js
@@ -1,11 +1,11 @@
 
 document.getElementById('sobreMi').onclick = function(){
+    console.log('Click en Sobre mi')
     document.getElementById('personalInfo').style = "display: none"
     document.getElementById('socialNetwork').style = "display: none"
     document.getElementById('myBlog').style = "display: none"
     document.getElementById('contact').style = "display: none"
     document.getElementById('aboutMe').style = "display: content"
-    
 }
 
 document.getElementById('infoPersonal').onclick = function(){
@@ -13,7 +13,7 @@ document.getElementById('infoPersonal').onclick = function(){
     document.getElementById('socialNetwork').style = "display: none"
     document.getElementById('myBlog').style = "display: none"
     document.getElementById('contact').style = "display: none"
-    document.getElementById('personalInfo').style = "display: contents"
+    document.getElementById('personalInfo').style = "display: content"
 }
 
 document.getElementById('redes').onclick = function(){
@@ -40,3 +40,32 @@ document.getElementById('contacto').onclick = function(){
     document.getElementById('contact').style = "display: content"
 }
 
+if(document.getElementById("btnEnviar")){
+    var modal = document.getElementById("miModal");
+    var btn = document.getElementById("btnEnviar");
+    var span = document.getElementsByClassName("close")[0];
+    var body = document.getElementsByTagName("body")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+        body.style.position = "static";
+        body.style.height = "100%";
+        body.style.overflow = "hidden";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+        body.style.position = "inherit";
+        body.style.height = "auto";
+        body.style.overflow = "visible";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            body.style.position = "inherit";
+            body.style.height = "auto";
+            body.style.overflow = "visible";
+        }
+    }
+}
